b4843d250186d1d958ade47a91f3b73557bf8eb6
Ji Smith <jihyunjung0219@gmail.com>
2020-08-04 17:54:07 -0500

    fixed few functions, added more variables and made it work

--- script.js
+++ script.js
@@ -1,29 +1,80 @@
 
+
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+
+// Write password to the #password input
+function writePassword() {
+  userInput ();
+  var password = makePasswords();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
+
+
 // set the variable lists
 var lowerCase = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
 var upperCase = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
 var specialCharacter = ['`','!','@','#','$','%','^','&','*','(',')','-','=','+'];
 var numeric = ['0','1','2','3','4','5','6','7','8','9'];
-
+var userChoice = [];
+var passwordsLength = 8;
 
 
 // ask user the choice of the length
-var passwordsLength = prompt ("Please type the length of passwords you want from 8 and 128");
+
+function userInput() {
+  passwordsLength = prompt ("Please type the length of passwords you want from 8 and 128");
+    if (passwordsLength < 8 || passwordsLength > 128 && typeof passwordsLength !== 'number') {
+        alert ("put the right number!!!!");
+      } else {
+    var userOption = confirm ("lower case yes or no")
+      if (userOption === true) {
+      userChoice = userChoice.concat(lowerCase);
+    }
+    var userOption2 = confirm ("Upper case yes or no")
+      if (userOption2 === true) {
+      userChoice = userChoice.concat(upperCase);
+    }
+    var userOption3 = confirm ("special character yes or no")
+      if (userOption3 === true) {
+      userChoice = userChoice.concat(specialCharacter);
+    }
+    var userOption4 = confirm ("number yes or no")
+      if (userOption4 === true) {
+      userChoice = userChoice.concat(numeric);
+    }
+  }
+}
+
+function makePasswords () {
+    var passwords;
+    for (var i = 0; i < passwordsLength; i++) {
+   passwords = passwords + userChoice[Math.floor((Math.random()*userChoice.length))];
+   }
+   return passwords;
+}
+
+
+
+/*
 
 // set the condition and check if the user put the correct input
-if (passwordsLength >= 8 && passwordsLength <= 128 && typeof passwordsLength === 'number') {
+if (passwordsLength >= 8 || passwordsLength <= 128 && typeof passwordsLength === 'number') {
   alert = ("Thank you, your passwords will be " + passwordsLength + " characters long!");
  } else {
   alert = ("Please type a nubmer between 8 and 128");
- };
-
-
-
-// ask user's choice of the lists
-var getRandomLowerCase = confirm ("Do you want Lower Case in your passwords? Yes for 'Ok' No for 'Cancel'");
-if (getRandomLowerCasec === true) {getRandomLowerCase = function () {
-  return lowerCase[Math.floor((Math.random()*lowerCase.length))];
+ }
 }
+
+
+// get user input
+
 
 
 
@@ -36,9 +87,43 @@ if (confirm("Press a button!")) {
 
 */
 
+// list of user's choice
+
+
+
+// ask user's choice of the lists'
+/*
+function userPreference () {
+  if(confirm("Do you want Lower Case in your passwords? Yes for 'Ok' No for 'Cancel'")) {
+    userChoice = userChoice.concat(lowerCase);
+  }
+
+  if(confirm("Do you want Upper Case in your passwords? Yes for 'Ok' No for 'Cancel'")) {
+    userChoice = userChoice.concat(upperCase);
+  }
+
+  if(confirm("Do you want Special Character in your passwords? Yes for 'Ok' No for 'Cancel'")) {
+    userChoice = userChoice.concat(specialCharacter);
+  }
+
+  if(confirm("Do you want Numbers in your passwords? Yes for 'Ok' No for 'Cancel'")) {
+    userChoice = userChoice.concat(numeric);
+  }
+}
+
+*/
+
+
+/*
+
 var getRandomUpperCase = confirm ("Do you want Lower Case in your passwords? Yes for 'Ok' No for 'Cancel'");
 if (getRandomUpperCase === true) {getRandomUpperrCase = function () {
   return upperCase[Math.floor((Math.random()*upperCase.length))];
+}
+
+var getRandomLowerCase = confirm ("Do you want Lower Case in your passwords? Yes for 'Ok' No for 'Cancel'");
+if (getRandomLowerCasec === true) {getRandomLowerCase = function () {
+  return lowerCase[Math.floor((Math.random()*lowerCase.length))];
 }
 
 var getRandomSpecialCharacter = confirm ("Do you want Lower Case in your passwords? Yes for 'Ok' No for 'Cancel'");
@@ -51,39 +136,4 @@ if (getRandomNumber === true) {getRandomNumber= function () {
   return numeric[Math.floor((Math.random()*numeric.length))];
 }
 
-/*
-// check if the user choose minimum one list
-else {
-  alert ("you need to choose at least one!")
-}
 */
-
-
-// if statment for each answers
-
-
-
-// generate passwords
-for (var i = 0; i === passwordsLength-1; i++) {
-  alert(password[i]);
-}
-
-
-// alert the passwords
-
-
-
-// Assignment Code
-var generateBtn = document.querySelector("#generate");
-
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-
-}
-
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
