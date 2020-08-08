2a7569dccadb2b0f8dbe5d3bf1ff3a2365682f43
Ji Smith <jihyunjung0219@gmail.com>
2020-08-07 20:52:05 -0500

    fixed error when typed outside range length

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  userInput ();
+  userInput();
   var password = makePasswords();
   var passwordText = document.querySelector("#password");
 
@@ -17,47 +17,54 @@ function writePassword() {
 generateBtn.addEventListener("click", writePassword);
 
 
-// set the variable lists
-var lowerCase = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
-var upperCase = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
-var specialCharacter = ['`','!','@','#','$','%','^','&','*','(',')','-','=','+'];
-var numeric = ['0','1','2','3','4','5','6','7','8','9'];
+// set the variable lists stored in different types
+var lowerCase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+var upperCase = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+var specialCharacter = ['`', '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '-', '=', '+'];
+var numeric = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
 var userChoice = [];
 var passwordsLength = 8;
 
 
-// ask user the choice of the length
+// ask user the choice of the length + types of characters
 
 function userInput() {
-  passwordsLength = prompt ("Please type the length of passwords you want from 8 and 128");
-    if (passwordsLength < 8 || passwordsLength > 128 && typeof passwordsLength !== 'number') {
-        alert ("put the right number!!!!");
-      } else {
-    var userOption = confirm ("Do you want Lower Case in your passwords? [Yes = 'Ok', No ='Cancel']")
-      if (userOption === true) {
+  passwordsLength = prompt("Please type the length of passwords you want from 8 and 128");
+  if (passwordsLength < 8 || passwordsLength > 128 && typeof passwordsLength !== 'number') {
+    alert("Please type the length of passwords you want from 8 and 128");
+    userInput();
+  } else {
+    var userOption = confirm("Do you want Lower Case in your passwords? [Yes = 'Ok', No ='Cancel']")
+    if (userOption === true) {
       userChoice = userChoice.concat(lowerCase);
     }
-    var userOption2 = confirm ("Do you want Upper Case in your passwords? [Yes = 'Ok', No ='Cancel']")
-      if (userOption2 === true) {
+    var userOption2 = confirm("Do you want Upper Case in your passwords? [Yes = 'Ok', No ='Cancel']")
+    if (userOption2 === true) {
       userChoice = userChoice.concat(upperCase);
     }
-    var userOption3 = confirm ("Do you want Special Character in your passwords? [Yes = 'Ok', No ='Cancel']")
-      if (userOption3 === true) {
+    var userOption3 = confirm("Do you want Special Character in your passwords? [Yes = 'Ok', No ='Cancel']")
+    if (userOption3 === true) {
       userChoice = userChoice.concat(specialCharacter);
     }
-    var userOption4 = confirm ("Do you want Numbers in your passwords? [Yes = 'Ok', No ='Cancel']")
-      if (userOption4 === true) {
+    var userOption4 = confirm("Do you want Numbers in your passwords? [Yes = 'Ok', No ='Cancel']")
+    if (userOption4 === true) {
       userChoice = userChoice.concat(numeric);
+    }
+    if (!userOption && !userOption2 && !userOption3 && !userOption4) {
+      alert("pick one");
+      userInput();
     }
   }
 }
 
-function makePasswords () {
-    var passwords;
-    for (var i = 0; i < passwordsLength; i++) {
-   passwords = passwords + userChoice[Math.floor((Math.random()*userChoice.length))];
-   }
-   return passwords;
+// make passwords
+function makePasswords() {
+  var passwords = '';
+  //loop through random characters
+  for (var i = 0; i < passwordsLength; i++) {
+    passwords += userChoice[Math.floor((Math.random() * userChoice.length))];
+  }
+  return passwords;
 }
 
 
